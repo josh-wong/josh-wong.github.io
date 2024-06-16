898f8e01fd856a7cb3021a4ec716f7a6f4c7f84a
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-06-16 21:04:08 +0900

    Add Typebot chatbot

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -3,8 +3,8 @@ import clsx from 'clsx';
 import {ThemeClassNames} from '@docusaurus/theme-common';
 import {useDoc} from '@docusaurus/theme-common/internal';
 import TagsListInline from '@theme/TagsListInline';
-
 import EditMetaRow from '@theme/EditMetaRow';
+import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2/dist/web.js';
 
 export default function DocItemFooter(): JSX.Element | null {
   const {metadata} = useDoc();
@@ -20,31 +20,42 @@ export default function DocItemFooter(): JSX.Element | null {
   }
 
   return (
-    <footer
-      className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
-      {/* Commented out tags since we've added them to the top of docs. (See src/theme/DocVersionBadge/index.tsx) */}
-      {/* {canDisplayTagsRow && (
-        <div
-          className={clsx(
-            'row margin-top--sm',
-            ThemeClassNames.docs.docFooterTagsRow,
-          )}>
-          <div className="col">
-            <TagsListInline tags={tags} />
+    <>
+      {
+        Typebot.initBubble({
+          typebot: 'faq-o3dwttn',
+          theme: {
+            button: { backgroundColor: 'var(--ifm-color-primary)' },
+          },"@typebot.io/js": "0.2.87",
+    "@typebot.io/react": "0.2.87",
+        })
+      }
+      <footer
+        className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
+        {/* Commented out tags since we've added them to the top of docs. (See src/theme/DocVersionBadge/index.tsx) */}
+        {/* {canDisplayTagsRow && (
+          <div
+            className={clsx(
+              'row margin-top--sm',
+              ThemeClassNames.docs.docFooterTagsRow,
+            )}>
+            <div className="col">
+              <TagsListInline tags={tags} />
+            </div>
           </div>
-        </div>
-      )} */}
-      {canDisplayEditMetaRow && (
-        <EditMetaRow
-          className={clsx(
-            'margin-top--sm',
-            ThemeClassNames.docs.docFooterEditMetaRow,
-          )}
-          editUrl={editUrl}
-          lastUpdatedAt={lastUpdatedAt}
-          lastUpdatedBy={lastUpdatedBy}
-        />
-      )}
-    </footer>
+        )} */}
+        {canDisplayEditMetaRow && (
+          <EditMetaRow
+            className={clsx(
+              'margin-top--sm',
+              ThemeClassNames.docs.docFooterEditMetaRow,
+            )}
+            editUrl={editUrl}
+            lastUpdatedAt={lastUpdatedAt}
+            lastUpdatedBy={lastUpdatedBy}
+          />
+        )}
+      </footer>
+    </>
   );
 }
