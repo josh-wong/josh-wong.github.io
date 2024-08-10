95b7cc3b98554e3d4d7df93351795c825ebfce94
Josh Wong <joshuarwong@gmail.com>
2024-08-10 11:25:19 +0900

    [Staging] Add Typebot-based chatbot (#64)
    * Create index.js
    
    * Add Typebot-based chatbot

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import FooterLinks from '@theme/Footer/Links';
+import FooterLogo from '@theme/Footer/Logo';
+import FooterCopyright from '@theme/Footer/Copyright';
+import FooterLayout from '@theme/Footer/Layout';
+import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.3/dist/web.js';
+
+function Footer() {
+  const {footer} = useThemeConfig();
+  if (!footer) {
+    return null;
+  }
+  const {copyright, links, logo, style} = footer;
+  return (
+    <>
+      <Typebot.initBubble
+        typebot="faq-o3dwttn"
+        theme={{
+          button: { backgroundColor: "#949acd", iconColor: "white" },
+          chatWindow: { maxHeight: "75%" },
+        }}
+      />
+      <FooterLayout
+        style={style}
+        links={links && links.length > 0 && <FooterLinks links={links} />}
+        logo={logo && <FooterLogo logo={logo} />}
+        copyright={copyright && <FooterCopyright copyright={copyright} />}
+      />
+    </>
+  );
+}
+
+<typebot-bubble style="width: 100%; height: 200px; "></typebot-bubble>
+
+export default React.memo(Footer);
