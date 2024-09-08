06cc28b5f816212435e7d0de4dee0d949722c625
Josh Wong <joshuarwong@outlook.com>
2024-09-08 22:18:51 +0900

    Remove `/` from `routeBasePath`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
       ({
         docs: {
           path: 'docs',
-          routeBasePath: '/',
+          routeBasePath: '',
           sidebarPath: './sidebars.js',
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
