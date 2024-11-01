2d6e25ad54c6867b1c32511c0df6d1cac1ffa323
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-11-02 01:19:45 +0900

    Fix URL to PR in notification

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -243,7 +243,7 @@ const config = {
             type: 'custom-NotificationBell',
             position: 'right',
             notifications: [
-              { id: 1, message: 'New feature!: Site notifications🚨', url: 'https://github.com/josh-wong/josh-wong.github.io/pull/118' },
+              { id: 1, message: 'New feature!: Site notifications🚨', url: 'https://github.com/josh-wong/josh-wong.github.io/pull/121' },
               { id: 2, message: 'Check out the unofficial Baird beer flavor quadrants🍺', url: '/baird-beer-quadrants' },
               { id: 3, message: 'Enjoy Baird beer? Ask the unofficial AI chatbot for recommendations🤖', url: 'https://typebot.co/baird-beer-recommendations-experimental-080f53' },
             ],
