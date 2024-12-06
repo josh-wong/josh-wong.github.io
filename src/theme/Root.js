bf21c51abcec6f10e6fba04c0d8e59aafba9fb89
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-12-06 17:32:14 +0900

    Add `@microsoft/clarity` code

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Clarity from '@microsoft/clarity';
+
+const projectId = "p9179dcazx"
+Clarity.init(projectId);
 
 // Default implementation, that you can customize
 export default function Root({children}) {
