eeb12ca61b38c65b51041589d06213c53ac07001
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-12-07 16:13:01 +0900

    Revise code for responding to cookie consent

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -5,21 +5,38 @@ import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-cons
 const projectId = "p9179dcazx"
 Clarity.init(projectId);
 
-window.addEventListener("CookieConsent", () => window.clarity('consent'));
+// window.addEventListener("CookieConsent", () => window.clarity('consent'));
 
 export default function Root({children}) {
-  return <>
-    {children}
-    <div>
-      <CookieConsent
-        location="bottom"
-        buttonText="I understand"
-        style={{ backgroundColor: "#080f53", padding: "20px" }}
-        buttonStyle={{ backgroundColor: "#fff", color: "#000", fontWeight: "500", fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'" }}
-        expires={150}
-      >
-        This website uses cookies to enhance the user experience. By continuing to use this website, you acknowledge that you have read and understood the cookie policy and consent to the use of cookies to improve your browsing experience, personalize content, and analyze website traffic.{" "}
-      </CookieConsent>
-    </div>
-  </>;
+  const handleConsentAccept = () => {
+    // Set the clarity consent and trigger the consent event
+    Clarity.consent(true);
+    console.log("Clarity consent granted.");
+  };
+
+  return (
+    <>
+      {children}
+      <div>
+        <CookieConsent
+          location="bottom"
+          buttonText="I understand"
+          style={{
+            backgroundColor:"#080f53",
+            padding: "20px"
+          }}
+          buttonStyle={{
+            backgroundColor: "#fff",
+            color: "#000",
+            fontWeight: "500",
+            fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"
+          }}
+          expires={150}
+          onAccept={handleConsentAccept}
+        >
+          This website uses cookies to enhance the user experience. By continuing to use this website, you acknowledge that you have read and understood the cookie policy and consent to the use of cookies to improve your browsing experience, personalize content, and analyze website traffic.{" "}
+        </CookieConsent>
+      </div>
+    </>
+  );
 }
