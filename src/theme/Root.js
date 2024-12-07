c1163017ca585a607fad6d1e8e009a4a9b51bb6c
Josh Wong <joshuarwong@gmail.com>
2024-12-07 17:26:28 +0900

    Add cookie consent banner (#155)
    * Add `react-cookie-consent`
    
    * Add cookie consent banner
    
    * Revise code for responding to cookie consent
    
    * Revise code for responding to cookie consent
    
    * Add link to cookie policy
    
    * Create index.mdx
    
    * Create index.mdx

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Clarity from '@microsoft/clarity';
+import CookieConsent from "react-cookie-consent";
 
-const projectId = "p9179dcazx"
-Clarity.init(projectId);
+// Initialize Clarity with the given project ID
+const projectId = "p9179dcazx";
 
-// Default implementation, that you can customize
-export default function Root({children}) {
-  return <>{children}</>;
+export default function Root({ children }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Initialize Clarity only on the client-side
+      Clarity.init(projectId);
+
+      // Add an event listener for the consent event
+      window.addEventListener("CookieConsent", () => {
+        if (window.clarity) {
+          window.clarity('consent');
+          console.log("Clarity consent event triggered.");
+        }
+      });
+    }
+  }, []);
+
+  const handleConsentAccept = () => {
+    // Dispatch a custom "CookieConsent" event when the user accepts cookies
+    const consentEvent = new Event("CookieConsent");
+    window.dispatchEvent(consentEvent);
+    console.log("CookieConsent event dispatched.");
+  };
+
+  return (
+    <>
+      {children}
+      <div>
+        <CookieConsent
+          location="bottom"
+          buttonText="I understand"
+          style={{
+            backgroundColor: "#080f53",
+            padding: "20px",
+          }}
+          buttonStyle={{
+            backgroundColor: "#fff",
+            color: "#000",
+            fontWeight: "500",
+            fontFamily:
+              "system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+          }}
+          expires={150}
+          onAccept={handleConsentAccept}
+        >
+          This website uses cookies to enhance the user experience. By continuing to use this website, you acknowledge that you have read and understood the <a href="/cookies">cookie policy</a> and consent to the use of cookies to improve your browsing experience, personalize content, and analyze website traffic.
+        </CookieConsent>
+      </div>
+    </>
+  );
 }
