743adb827915c1149c8b5bbfcdba748698667490
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-12-20 01:25:12 +0900

    Enable index

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,14 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/blog/">
+            Blog
+          </Link>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link
+            className="button button--secondary button--lg"
+            to="/projects/">
+            Projects
           </Link>
         </div>
       </div>
