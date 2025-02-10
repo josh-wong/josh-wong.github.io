josh-wong/josh-wong.github.io

commit 0e3e7c4727fd8589c67d5ab581ab2be660353794

Add `blog/` to blog post URLs and `projects/` to project documentation URLs; enable base URL home page  (#162)
* Enable index

* Move file

* Change links

* Add `projects` sidebar

* Change links for blog and projects

* Disable description

docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
       ({
         docs: {
           path: 'docs',
-          routeBasePath: '/',
+          routeBasePath: '/projects',
           sidebarPath: './sidebars.js',
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
@@ -67,12 +67,16 @@ const config = {
         blog: {
           blogSidebarTitle: 'All posts',
           blogSidebarCount: 'ALL',
+          postsPerPage: 5,
           feedOptions: {
             type: 'all',
+            title: '',
+            description: '',
             copyright: 'Copyright © ${new Date().getFullYear()} 080f53',
             limit: null,
           },
-          routeBasePath: '/',
+          routeBasePath: 'blog',
+          path: 'blog',
           showReadingTime: true,
           onUntruncatedBlogPosts: 'ignore',          
           // Please change this to your repo.
@@ -227,7 +231,11 @@ const config = {
           //   position: 'left',
           //   label: 'Tutorial',
           // },
-          {to: '/', label: 'Blog', position: 'left'},
+          {
+            to: '/blog',
+            label: 'Blog',
+            position: 'left'
+          },
           {
             type: 'dropdown',
             label: 'Projects',
@@ -273,12 +281,12 @@ const config = {
               {
                 id: 3,
                 message: 'New feature!: Site notifications🚨',
-                url: '/2024/11/20/site-notification-feature'
+                url: '/blog/2024/11/20/site-notification-feature'
               },
               {
                 id: 2,
                 message: 'Check out the unofficial Baird beer flavor quadrants🍺',
-                url: '/baird-beer-quadrants'
+                url: '/projects/baird-beer-quadrants'
               },
               {
                 id: 1,
@@ -323,7 +331,7 @@ const config = {
             title: 'Projects',
             items: [
               {
-                label: 'Personal samples',
+                label: 'Documentation',
                 to: '/projects',
               },
             ],

docs/index.mdx
@@ -1,3 +1,7 @@
+---
+displayed_sidebar: projects
+---
+
 # Projects
 
 import { CardRowPersonalProjects } from '/src/components/Cards/projects';

sidebars.js
@@ -29,6 +29,39 @@ const sidebars = {
   ],
   */
 
+  projects: [
+    {
+      type: 'doc',
+      label: 'Baird Brewing beer profile quadrants',
+      id: 'baird-beer-quadrants/index',
+    },
+    {
+      type: 'doc',
+      label: 'Bitcoin Cash Node on a Raspberry Pi 4',
+      id: 'bitcoin-cash-node-on-raspberry-pi/index',
+    },
+    {
+      type: 'doc',
+      label: 'Music collection',
+      id: 'music-collection/index',
+    },
+    {
+      type: 'doc',
+      label: 'Microsoft Zune device and Zune software setup guide',
+      id: 'zune-software-setup/index',
+    },
+    {
+      type: 'doc',
+      label: 'passgen',
+      id: 'passgen/index',
+    },
+    {
+      type: 'link',
+      label: 'AI chatbot for beer recommendations (unofficial)',
+      href: 'https://signal.art/addstickers/#pack_id=183a3ca8d7ccdcdb8fa7728b17453fbc&pack_key=e9ac42b0e7276edd92d293321d2e51cca64e5744bad567fd9579b51abb78773d',
+    },
+  ],
+
   bairdBeerQuadrants: [
     {
       type: 'doc',

src/components/Cards/projects.tsx
@@ -68,7 +68,7 @@ const CardsPersonalProjects = [
     name: 'Baird beer profile quadrants🍻',
     // image: '<LINK_TO>.png',
     url: {
-      page: '../../baird-beer-quadrants',
+      page: '/projects/baird-beer-quadrants',
     },
     description: (
       <Translate id="personal.bairdBeerQuadrants.description">
@@ -80,7 +80,7 @@ const CardsPersonalProjects = [
     name: 'Bitcoin Cash Node on a Raspberry Pi🪙',
     // image: '<LINK_TO>.png',
     url: {
-      page: '../../bitcoin-cash-node-on-raspberry-pi',
+      page: '/projects/bitcoin-cash-node-on-raspberry-pi',
     },
     description: (
       <Translate id="personal.raspiBch.description">
@@ -92,7 +92,7 @@ const CardsPersonalProjects = [
     name: 'Microsoft Zune device and software setup🎵',
     // image: '<LINK_TO>.png',
     url: {
-      page: '../../zune-software-setup',
+      page: '/projects/zune-software-setup',
     },
     description: (
       <Translate id="personal.zuneSetup.description">
@@ -104,7 +104,7 @@ const CardsPersonalProjects = [
     name: 'passGen🔐',
     // image: '<LINK_TO>.png',
     url: {
-      page: '../../passgen',
+      page: '/projects/passgen',
     },
     description: (
       <Translate id="personal.passgen.description">

src/components/HomepageFeatures/_index.js
@@ -1,64 +1,0 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
-
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
-export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-}

src/components/HomepageFeatures/index.js
@@ -1,0 +1,64 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+// const FeatureList = [
+//   {
+//     title: 'Easy to Use',
+//     //Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+//     description: (
+//       <>
+//         Docusaurus was designed from the ground up to be easily installed and
+//         used to get your website up and running quickly.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Focus on What Matters',
+//     //Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+//     description: (
+//       <>
+//         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+//         ahead and move your docs into the <code>docs</code> directory.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Powered by React',
+//     //Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+//     description: (
+//       <>
+//         Extend or customize your website layout by reusing React. Docusaurus can
+//         be extended while reusing the same header and footer.
+//       </>
+//     ),
+//   },
+// ];
+
+// function Feature({title, description}) {
+//   return (
+//     <div className={clsx('col col--4')}>
+//       <div className="text--center">
+//         {/* <Svg className={styles.featureSvg} role="img" /> */}
+//       </div>
+//       <div className="text--center padding-horiz--md">
+//         <Heading as="h3">{title}</Heading>
+//         <p>{description}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {/* {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))} */}
+        </div>
+      </div>
+    </section>
+  );
+}

src/pages/index.js
@@ -19,8 +19,14 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/blog/">
+            Blog
+          </Link>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link
+            className="button button--secondary button--lg"
+            to="/projects/">
+            Projects
           </Link>
         </div>
       </div>
@@ -33,7 +39,8 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      // description="Description will go into a meta tag in <head />"
+      >
       <HomepageHeader />
       <main>
         <HomepageFeatures />

commit cac325f457343e0cf12ae26bf7237ea310bf107a

Fix broken RSS link
This link became broken in #162 after some structural changes to the Docusaurus configuration file.

docusaurus.config.js
@@ -288,7 +288,7 @@ const config = {
           },
           {
             className: 'header-rss-link',
-            href: 'https://www.080f53.com/rss.xml',
+            href: 'https://www.080f53.com/blog/rss.xml',
             position: 'right',
             'aria-label': 'RSS',
           },